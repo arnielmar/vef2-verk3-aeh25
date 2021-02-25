6620ab4831f64b70296efc27f1a7a5699cc524f5
arnielmar <aeh25@hi.is>
2021-02-25 09:53:44 +0000

    Tafla fyrir users bætt við

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,3 +1,6 @@
+DROP TABLE IF EXISTS signatures;
+DROP TABLE IF EXISTS users;
+
 CREATE TABLE IF NOT EXISTS signatures(
   id serial primary key,
   name varchar(128) not null,
@@ -5,4 +8,13 @@ CREATE TABLE IF NOT EXISTS signatures(
   comment varchar(400) not null,
   anonymous boolean not null default true,
   signed timestamp with time zone not null default current_timestamp
+);
+
+CREATE TABLE IF NOT EXISTS users(
+  id serial primary key,
+  username character varying(255) unique not null,
+  password character varying(255) not null,
+  admin boolean default false,
+  created timestamp with time zone not null default current_timestamp,
+  updated timestamp with time zone not null default current_timestamp
 );
